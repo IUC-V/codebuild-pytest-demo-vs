327987912ec792dadcac4ed71da46ed19049289d
IUC-V <87781501+IUC-V@users.noreply.github.com>
2025-03-06 00:06:57 +0530

    Update test_ops.py

--- test_ops.py
+++ test_ops.py
@@ -1,13 +1,13 @@
 from ops import *
 
 def test_add():
-    assert add(2,3) == 5
+    assert add(2,3) == 8
 
 def test_subtract():
     assert subtract(2, 3) == -1
 
 def test_multiply():
-    assert multiply(2, 3) == 6
+    assert multiply(2, 3) == 2
 
 def test_divide():
     assert divide(10,5) == 2
